0a077f577429f3b2c035453a8cfe5dbd8c572e01
Manish <van.manish@gmail.com>
2019-12-22 01:33:13 +0530

    valid parenthesis

---
+++ src/ValidParentheses.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        ValidParentheses parentheses = new ValidParentheses();
+//        System.out.println(parentheses.isValid("{[]}"));
+        System.out.println(parentheses.isValid("}{[]"));
+    }
+/*  Input: "()[]{}"
+    Output: true
+    Example 3:
+
+    Input: "(]"
+    Output: false
+    Example 4:
+
+    Input: "([)]"
+    Output: false
+    Example 5:
+
+    Input: "{[]}"
+    Output: true */
+
+    public boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        map.put('(', ')'); map.put('[', ']'); map.put('{', '}');
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+
+            if (map.keySet().contains(curr)) {
+                stack.push(curr);
+            } else if (map.values().contains(curr)) {
+                if (!stack.empty() && map.get(stack.peek()) == curr) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+}
